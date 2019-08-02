b4c9d73b6af87d91fa263485ef49ef7a988237ec
CMLDMR <cemaldemir07@gmail.com>
2019-08-02 19:29:10 +0300

    Kategori Listeleme ve Oluşturma Widget Eklendi.

--- stok/stokkategoridialog.cpp
+++ stok/stokkategoridialog.cpp
@@ -1,14 +1,46 @@
 #include "stokkategoridialog.h"
 #include "ui_stokkategoridialog.h"
+#include "base/stokkategori.h"
+#include <QStandardItemModel>
 
-StokKategoriDialog::StokKategoriDialog(QWidget *parent) :
+StokKategoriDialog::StokKategoriDialog(mongocxx::database *_db, QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::StokKategoriDialog)
+    ui(new Ui::StokKategoriDialog),
+    DBClass (_db)
 {
     ui->setupUi(this);
+
+    mKategoriModel = new QStandardItemModel();
+    ui->listView_KategoriList->setModel(mKategoriModel);
+
+    this->initList();
+
 }
 
 StokKategoriDialog::~StokKategoriDialog()
 {
+    delete mKategoriModel;
     delete ui;
+}
+
+void StokKategoriDialog::on_pushButton_YeniKaydet_clicked()
+{
+    auto coll = this->db()->collection(STOKKATEGORI::KATEGORICOLLECTION);
+    auto kat = STOKKATEGORI::StokKategori::Create_Kategori(&coll,ui->lineEdit_YeniStokKategroi->text());
+
+    this->initList();
+}
+
+void StokKategoriDialog::initList()
+{
+
+    auto coll = this->db()->collection(STOKKATEGORI::KATEGORICOLLECTION);
+    auto list = STOKKATEGORI::StokKategori::GetKategoriList(&coll);
+
+    mKategoriModel->removeRows(0,mKategoriModel->rowCount());
+
+    for( auto item : list )
+    {
+        mKategoriModel->insertRow(0,item);
+    }
 }
--- stok/stokkategoridialog.h
+++ stok/stokkategoridialog.h
@@ -2,21 +2,33 @@
 #define STOKKATEGORIDIALOG_H
 
 #include <QDialog>
+#include "base/dbclass.h"
+
+class QStandardItemModel;
 
 namespace Ui {
 class StokKategoriDialog;
 }
 
-class StokKategoriDialog : public QDialog
+class StokKategoriDialog : public QDialog , public DBClass
 {
     Q_OBJECT
 
 public:
-    explicit StokKategoriDialog(QWidget *parent = nullptr);
+    explicit StokKategoriDialog(mongocxx::database* _db , QWidget *parent = nullptr);
     ~StokKategoriDialog();
+
+private slots:
+    void on_pushButton_YeniKaydet_clicked();
 
 private:
     Ui::StokKategoriDialog *ui;
+
+    QStandardItemModel* mKategoriModel;
+
+    void initList();
+
+
 };
 
 #endif // STOKKATEGORIDIALOG_H
