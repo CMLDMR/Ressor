9c966c550f639598f8a8624609467db4a437d703
CMLDMR <cemaldemir07@gmail.com>
2019-08-02 19:39:37 +0300

    StokBirim Eklendi.

---
+++ stok/base/stokbirim.cpp
@@ -0,0 +1,6 @@
+#include "stokbirim.h"
+
+StokBirim::StokBirim()
+{
+
+}
---
+++ stok/base/stokbirim.h
@@ -0,0 +1,11 @@
+#ifndef STOKBIRIM_H
+#define STOKBIRIM_H
+
+
+class StokBirim : public DBClass
+{
+public:
+    StokBirim();
+};
+
+#endif // STOKBIRIM_H
