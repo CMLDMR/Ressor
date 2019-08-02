9b685801b49d241c84e412fbdb6818512367f006
CMLDMR <cemaldemir07@gmail.com>
2019-08-02 21:00:55 +0300

    YeniStokBirimEkleDialog Tamamlandı.

--- stok/yenistokbirimekledialog.cpp
+++ stok/yenistokbirimekledialog.cpp
@@ -1,14 +1,78 @@
 #include "yenistokbirimekledialog.h"
 #include "ui_yenistokbirimekledialog.h"
 
-YeniStokBirimEkleDialog::YeniStokBirimEkleDialog(QWidget *parent) :
+#include <QStandardItemModel>
+
+#include "base/stokbirim.h"
+
+YeniStokBirimEkleDialog::YeniStokBirimEkleDialog(mongocxx::database *_db, QWidget *parent) :
     QDialog(parent),
+    DBClass (_db),
     ui(new Ui::YeniStokBirimEkleDialog)
 {
     ui->setupUi(this);
+
+    mModel = new QStandardItemModel();
+
+    ui->listView->setModel(mModel);
+
+    this->initList();
+
+
 }
 
 YeniStokBirimEkleDialog::~YeniStokBirimEkleDialog()
 {
+    delete mModel;
     delete ui;
+}
+
+void YeniStokBirimEkleDialog::on_pushButton_YeniBirimEkle_clicked()
+{
+
+
+    auto stokBirim = StokBirim::StokBirim::CreateStokBirim(this->db(),ui->lineEdit_YeniBirimAdi->text());
+
+
+    if( stokBirim )
+    {
+        std::cout << "VALUE" << std::endl;
+    }else{
+        std::cout << "NOVALUE() " << std::endl;
+    }
+
+
+    if( stokBirim.has_value() )
+    {
+        std::cout << stokBirim->getBirimAdi().toStdString() << std::endl;
+        std::cout << stokBirim->getBirimOid().to_string() << std::endl;
+    }else{
+        std::cout << "NOVALUE" << std::endl;
+    }
+
+
+    this->initList();
+
+
+
+
+
+
+
+}
+
+void YeniStokBirimEkleDialog::initList()
+{
+    auto cursor = StokBirim::StokBirim::GetStokBirimList(this->db());
+    mModel->removeRows(0,mModel->rowCount());
+    for( auto item : cursor )
+    {
+        std::cout << item->getBirimAdi().toStdString() << std::endl;
+
+        mModel->insertRow(0,item->newQStandardItem());
+        //        if( item )
+//        {
+//
+//        }
+    }
 }
--- stok/yenistokbirimekledialog.h
+++ stok/yenistokbirimekledialog.h
@@ -2,21 +2,32 @@
 #define YENISTOKBIRIMEKLEDIALOG_H
 
 #include <QDialog>
+#include "base/dbclass.h"
+
+
+class QStandardItemModel;
 
 namespace Ui {
 class YeniStokBirimEkleDialog;
 }
 
-class YeniStokBirimEkleDialog : public QDialog
+class YeniStokBirimEkleDialog : public QDialog , public DBClass
 {
     Q_OBJECT
 
 public:
-    explicit YeniStokBirimEkleDialog(QWidget *parent = nullptr);
+    explicit YeniStokBirimEkleDialog(mongocxx::database* _db , QWidget *parent = nullptr);
     ~YeniStokBirimEkleDialog();
+
+private slots:
+    void on_pushButton_YeniBirimEkle_clicked();
 
 private:
     Ui::YeniStokBirimEkleDialog *ui;
+
+    QStandardItemModel* mModel;
+
+    void initList();
 };
 
 #endif // YENISTOKBIRIMEKLEDIALOG_H
