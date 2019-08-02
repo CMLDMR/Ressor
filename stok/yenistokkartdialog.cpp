d35759d128cbe02e68d90fdec6beefbf1f1d1052
CMLDMR <cemaldemir07@gmail.com>
2019-08-02 23:40:52 +0300

    YeniStokKartDialog Stok Kart Tanımlama Tamamlandi.

--- stok/yenistokkartdialog.cpp
+++ stok/yenistokkartdialog.cpp
@@ -5,6 +5,7 @@
 #include "base/stokkart.h"
 #include "stokkategoridialog.h"
 #include "yenistokbirimekledialog.h"
+#include "base/stokbirim.h"
 #include "base/stokkategori.h"
 
 #include <QMessageBox>
@@ -28,6 +29,14 @@ YeniStokKartDialog::YeniStokKartDialog(mongocxx::database *_db, QWidget *parent)
     }
 
 
+    auto BirList = StokBirim::StokBirim::GetStokBirimList(this->db());
+
+    ui->comboBox_StokBirimi->clear();
+    for( auto item : BirList )
+    {
+        ui->comboBox_StokBirimi->addItem(item->getBirimAdi(),item->getBirimOid().to_string().c_str());
+    }
+
 
 }
 
@@ -38,38 +47,89 @@ YeniStokKartDialog::~YeniStokKartDialog()
 
 void YeniStokKartDialog::on_pushButton_StokKaydet_clicked()
 {
-    auto coll = this->db()->collection(STOKKARTKEY::STOKCOLLECTION);
-    auto stokKart = StokKart::Create_StokKart(coll);
+//    auto coll = this->db()->collection(STOKKARTKEY::STOKCOLLECTION);
+    auto stokKart = StokKart::Create_StokKart(this->db());
 
-    bool saveError = false;
-
-    if( !stokKart.setKartAdi(ui->lineEdit_StokAdi->text()) )
+    if( stokKart )
     {
-        saveError = true;
-    }
 
-    if( !stokKart.setStokKodu(ui->lineEdit_StokKodu->text()) )
-    {
-        saveError = true;
-    }
+        bool saveError = false;
 
 
+        if( !stokKart->setKartAdi(ui->lineEdit_StokAdi->text()) )
+        {
+            saveError = true;
+        }
 
-    if( saveError )
-    {
+
+        if( !stokKart->setStokKodu(ui->lineEdit_StokKodu->text()) )
+        {
+            saveError = true;
+        }
+
+
+        if( !stokKart->setKategori(ui->comboBox_StokKategori->currentText()) )
+        {
+            saveError = true;
+        }
+
+
+        if( !stokKart->setBirim(ui->comboBox_StokBirimi->currentText()) )
+        {
+            saveError = true;
+        }
+
+
+        if( !stokKart->setAlisFiyati(ui->doubleSpinBox_AlisFiyati->value()) )
+        {
+            saveError = true;
+        }
+
+        if( !stokKart->setKDVOrani(ui->doubleSpinBox_KDV->value()) )
+        {
+            saveError = true;
+        }
+
+        if( !stokKart->setOTVOrani(ui->doubleSpinBox_OTV->value()) )
+        {
+            saveError = true;
+        }
+
+        if( !stokKart->setSatisFiyati(ui->doubleSpinBox_SatisFiyati->value()) )
+        {
+            saveError = true;
+        }
+
+
+
+
+        if( saveError )
+        {
+            QMessageBox msg;
+            msg.setText("Stok Kartı Oluşturuldu Ancak Bazi Bilgiler Girilemedi");
+            msg.setIcon(QMessageBox::Icon::Warning);
+            msg.setWindowTitle("UYARI");
+            msg.exec();
+        }else{
+            QMessageBox msg;
+            msg.setText("Stok Kartı oluşturuldu");
+            msg.setIcon(QMessageBox::Icon::Information);
+            msg.setWindowTitle("Bilgi");
+            msg.exec();
+            this->close();
+        }
+    }else{
         QMessageBox msg;
         msg.setText("Stok Kartı Oluşturulamadı");
         msg.setIcon(QMessageBox::Icon::Warning);
         msg.setWindowTitle("UYARI");
         msg.exec();
-    }else{
-        QMessageBox msg;
-        msg.setText("Stok Kartı oluşturuldu");
-        msg.setIcon(QMessageBox::Icon::Information);
-        msg.setWindowTitle("Bilgi");
-        msg.exec();
-        this->close();
     }
+
+
+
+
+
 }
 
 void YeniStokKartDialog::on_pushButton_YeniKategoriEkle_clicked()
