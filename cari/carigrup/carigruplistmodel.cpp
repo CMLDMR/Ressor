9202927d49be94e736a659e46c5d0cf156f1d326
CMLDMR <cemaldemir07@gmail.com>
2019-08-14 18:07:19 +0300

    CariGrupListModel Oluşturuldu.

---
+++ cari/carigrup/carigruplistmodel.cpp
@@ -0,0 +1,6 @@
+#include "carigruplistmodel.h"
+
+CariGrupListModel::CariGrupListModel()
+{
+
+}
---
+++ cari/carigrup/carigruplistmodel.h
@@ -0,0 +1,11 @@
+#ifndef CARIGRUPLISTMODEL_H
+#define CARIGRUPLISTMODEL_H
+
+
+class CariGrupListModel : public QStandardItemModel
+{
+public:
+    CariGrupListModel();
+};
+
+#endif // CARIGRUPLISTMODEL_H
