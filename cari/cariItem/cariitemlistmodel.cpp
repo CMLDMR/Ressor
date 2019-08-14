12f1fad62b749c3722edb2a7bcb4ac54046c9bb7
CMLDMR <cemaldemir07@gmail.com>
2019-08-14 20:15:25 +0300

    CariItemListModel Eklendi.

---
+++ cari/cariItem/cariitemlistmodel.cpp
@@ -0,0 +1,6 @@
+#include "cariitemlistmodel.h"
+
+CariItemListModel::CariItemListModel()
+{
+
+}
---
+++ cari/cariItem/cariitemlistmodel.h
@@ -0,0 +1,11 @@
+#ifndef CARIITEMLISTMODEL_H
+#define CARIITEMLISTMODEL_H
+
+
+class CariItemListModel : public QStandardItemModel
+{
+public:
+    CariItemListModel();
+};
+
+#endif // CARIITEMLISTMODEL_H
