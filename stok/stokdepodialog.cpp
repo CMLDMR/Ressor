e58e1286a9c77a2d648be4df341408db6eb37f4b
CMLDMR <cemaldemir07@gmail.com>
2019-08-17 04:06:31 +0300

    StokDepoDialog Oluşturuldu.

--- stok/stokdepodialog.cpp
+++ stok/stokdepodialog.cpp
@@ -1,14 +1,24 @@
 #include "stokdepodialog.h"
 #include "ui_stokdepodialog.h"
 
-StokDepoDialog::StokDepoDialog(QWidget *parent) :
+StokDepoDialog::StokDepoDialog(mongocxx::database *_db, QWidget *parent) :
     QDialog(parent),
+    DBClass (_db),
     ui(new Ui::StokDepoDialog)
 {
     ui->setupUi(this);
+
+    mModel = new StokDepoModel(this->db());
+
+    ui->listView->setModel(mModel);
 }
 
 StokDepoDialog::~StokDepoDialog()
 {
     delete ui;
+}
+
+void StokDepoDialog::on_pushButton_Ekle_clicked()
+{
+    mModel->initModel();
 }
--- stok/stokdepodialog.h
+++ stok/stokdepodialog.h
@@ -2,21 +2,27 @@
 #define STOKDEPODIALOG_H
 
 #include <QDialog>
+#include "base/stokdepomodel.h"
 
 namespace Ui {
 class StokDepoDialog;
 }
 
-class StokDepoDialog : public QDialog
+class StokDepoDialog : public QDialog , public DBClass
 {
     Q_OBJECT
 
 public:
-    explicit StokDepoDialog(QWidget *parent = nullptr);
+    explicit StokDepoDialog(mongocxx::database* _db , QWidget *parent = nullptr);
     ~StokDepoDialog();
+
+private slots:
+    void on_pushButton_Ekle_clicked();
 
 private:
     Ui::StokDepoDialog *ui;
+
+    StokDepoModel* mModel;
 };
 
 #endif // STOKDEPODIALOG_H
